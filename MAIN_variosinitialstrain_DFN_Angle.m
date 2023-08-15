d3ecfab22dc253b3d2d4b517bf4947234f821a5f
sobstfeld <sobstfeld@itba.edu.ar>
2023-08-15 12:17:02 -0300

    commit

--- MAIN_variosinitialstrain_DFN_Angle.m
+++ MAIN_variosinitialstrain_DFN_Angle.m
@@ -10,6 +10,6 @@ strain0(1:2,:)=[-6 -5 -4 -3 -2 -1.5;
             
 for i=1:nCasos
      initialStrainExtS=strain0(:,i);
-     nombreCorrida=['DFN_Angle_github_prueba_' num2str(i)];
+     nombreCorrida=['DFN_Angle_github_' num2str(i)];
      mainDfit_rev082023_variosInitialStrain
 end
